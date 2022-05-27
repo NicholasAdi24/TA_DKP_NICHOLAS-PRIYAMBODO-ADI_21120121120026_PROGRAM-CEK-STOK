1aa4d78c97f325b9ca795e9abcbd55d4a19b6b5b
NicholasAdi24 <102377198+NicholasAdi24@users.noreply.github.com>
2022-05-27 16:51:14 +0700

    Create main.java

---
+++ main.java
@@ -0,0 +1,20 @@
+
+package tugasakhir;
+
+
+public class Tugasakhir {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Login lg = new Login();
+        lg.setVisible(true);
+        lg.pack();
+        lg.setLocationRelativeTo(null);
+        lg.setDefaultCloseOperation(Login.EXIT_ON_CLOSE);
+        
+
+    }
+    
+}
